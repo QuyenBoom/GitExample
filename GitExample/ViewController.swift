41660433b3ccfe2397f268602da3e1bab65672a3
Quyen Khuc <s3618696@rmit.edu.vn>
2018-08-28 11:28:36 +0700

    Initial Commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Quyen, Khuc Thi Xuan on 8/28/18.
+//  Copyright © 2018 Quyen, Khuc Thi Xuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
